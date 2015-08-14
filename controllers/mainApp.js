283b3d720dd92d6e80ea6e4e15f7245434cb2d2a
James Jordan <JamesCyrilJordan@Outlook.com>
2015-08-14 13:56:53 -0400

    Added footer, nav, and various script files
    Setup footerModule structure.
    Setup navModule structure.
    Added various Angular scripts and linked them in index.html

---
+++ controllers/mainApp.js
@@ -0,0 +1,52 @@
+﻿angular.module('mainApp', ['eventModule', 'navModule', 'ngRoute', 'footerModule'])
+    .config([
+        '$routeProvider',
+        function ($routeProvider) {
+        	//console.log($routeProvider);
+        	$routeProvider.
+				 when('/home', {
+				 	templateUrl: 'home.html',
+				 	controller: 'HomeCtrl'
+				 }).
+				 when('/about', {
+				 	templateUrl: 'about.html',
+				 	controller: 'AboutCtrl'
+				 }).
+				 when('/contact', {
+				 	templateUrl: 'contact.html',
+				 	controller: 'ContactCtrl'
+				 }).
+				 when('/portfolio', {
+				 	templateUrl: 'portfolio.html',
+				 	controller: 'PortCtrl'
+				 }).
+				 when('/blog', {
+				 	templateUrl: 'blog/blog.html',
+				 	controller: 'BlogCtrl'
+				 }).
+				 when('/post/:id', {
+				 	templateUrl: 'blog/post.html',
+				 	controller: 'PostCtrl'
+				 }).
+				 when('/createBlog', {
+				 	templateUrl: 'blog/createBlog.html',
+				 	controller: 'CreateBlog'
+				 }).
+				 otherwise({
+				 	redirectTo: '/home'
+				 });
+        	//console.log('Configuration hook');
+        }
+    ])
+    .run([
+        function () {
+        	/* Run is when the app gets kicked off*/
+        	//console.log('Run hook');
+        }
+    ])
+    // controllers
+    .controller('HomeCtrl', [
+        '$scope', function ($scope) {
+        	console.log('Home Ctrl Loaded');
+        }
+    ]);
