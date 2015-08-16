496a282d429c0de21e7c66b6c920a584ff58b852
James Jordan <JamesCyrilJordan@Outlook.com>
2015-08-16 19:14:10 -0400

    Added sorting to contact list

--- controllers/contactModule.js
+++ controllers/contactModule.js
@@ -10,39 +10,49 @@
 				//console.log("Contact Module::running");
 			}
 		])
-		.controller('ContactViewCtrl', ['$scope', '$http',
-			function ($scope, $http) {
-				var req = {
-					method: 'GET',
-					url: 'http://challenge.acstechnologies.com/api/contact/',
-					headers: {
-						'X-Auth-Token': 'pkNClPIgOp1Oo3mRmIAJRwVL9QuIpbSLsaYKZ08w',
-						'Origin': undefined
-					}
+		.controller('ContactViewCtrl', ['$scope', '$http', function ($scope, $http) {
+
+			//#region Get Contact List JSON
+			// setup http headers, method, etc...
+			var req = {
+				method: 'GET',
+				url: 'http://challenge.acstechnologies.com/api/contact/',
+				headers: {
+					'X-Auth-Token': 'pkNClPIgOp1Oo3mRmIAJRwVL9QuIpbSLsaYKZ08w',
+					'Origin': undefined
+				}
+			};
+			// Get Contact List and assign to scope
+			$http(req).then(function (response) {
+				$scope.contactList = response.data.data;
+				$scope.orderByField = 'first_name';
+				$scope.reverseSort = false;
+				//#region Paging - Paging for contact list
+				$scope.itemsPerPage = 10;
+				$scope.currentPage = 1;
+				$scope.totalItems = $scope.contactList.length;
+
+				$scope.pageCount = function () {
+					return Math.ceil($scope.contactList.length / $scope.itemsPerPage);
 				};
-				$http(req).then(function (response) {
-					$scope.contactList = response.data.data;
 
-					//#region Paging - Paging for contact list
-					$scope.itemsPerPage = 5;
-					$scope.currentPage = 1;
-					$scope.totalItems = $scope.contactList.length;
-
-					$scope.pageCount = function () {
-						return Math.ceil($scope.contactList.length / $scope.itemsPerPage);
-					};
-
-					$scope.$watch('currentPage + itemsPerPage', function () {
-						var begin = (($scope.currentPage - 1) * $scope.itemsPerPage), end = begin + $scope.itemsPerPage;
-						$scope.filteredContacts = $scope.contactList.slice(begin, end);
-					});
-
-					//#endregion
-				}, function (response) {
-					alert('fail');
+				$scope.$watch('currentPage + itemsPerPage', function () {
+					var begin = (($scope.currentPage - 1) * $scope.itemsPerPage), end = begin + $scope.itemsPerPage;
+					$scope.filteredContacts = $scope.contactList.slice(begin, end);
 				});
+				//#endregion
 
-			}
+
+			}, function (response) {
+				alert('fail');
+			})
+			.controller('ContactCreateCtrl', ['$scope', '$http', function ($scope, $http) {
+				
+			}]);
+			//#endregion
+
+
+
+		}
 		]);
-
 })();
